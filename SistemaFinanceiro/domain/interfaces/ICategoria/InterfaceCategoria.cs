50ad46b9eecc3c80abaacf571cd2af05b4f30130
Arthur Mendes <arthurdev1998@gmail.com>
2023-09-02 11:18:48 -0300

    criacao das interfaces

---
+++ SistemaFinanceiro/domain/interfaces/ICategoria/InterfaceCategoria.cs
@@ -0,0 +1,13 @@
+﻿using domain.interfaces.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain.interfaces.ICategoria
+{
+    public interface InterfaceCategoria 
+    {
+    }
+}
