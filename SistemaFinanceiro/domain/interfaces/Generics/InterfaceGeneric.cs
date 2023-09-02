9db8ff0b6ef19e078e06677147f4cff5cf20b21e
Arthur Mendes <arthurdev1998@gmail.com>
2023-09-02 10:32:22 -0300

    criacao da interfaceGeneric para modelar os Cruds

---
+++ SistemaFinanceiro/domain/interfaces/Generics/InterfaceGeneric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain.interfaces.Generics
+{
+    public interface InterfaceGeneric<T> where T : class
+    {
+        Task Update(T Objeto);
+        Task Delete(T Objeto);
+        Task<T> GetEntityById(int Objeto);
+        Task <List<T>> List();
+
+    }
+}
